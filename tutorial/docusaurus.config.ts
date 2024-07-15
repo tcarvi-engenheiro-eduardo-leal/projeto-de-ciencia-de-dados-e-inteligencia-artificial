a2b4ec15dffcde8d52c2865b9c93c9b3c5a29b57
tcarvi <tcarvi@me.com>
2024-07-15 05:48:22 -0300

    Update

--- tutorial/docusaurus.config.ts
+++ tutorial/docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/tcarvi-engenheiro-eduardo-leal/python-coding',
+            'https://github.com/tcarvi-engenheiro-eduardo-leal/python-coding/edit/main/tutorial/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/tcarvi-engenheiro-eduardo-leal/python-coding',
+            'https://github.com/tcarvi-engenheiro-eduardo-leal/python-coding/edit/main/tutorial/',
         },
         theme: {
           customCss: './src/css/custom.css',
